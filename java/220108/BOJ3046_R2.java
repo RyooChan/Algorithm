943460fed77c1deb8b2ba21ad7470e4c747adcfc
RyooChan <53744363+RyooChan@users.noreply.github.com>
2022-01-09 20:31:36 +0900

    Create BOJ3046_R2.java

---
+++ java/220108/BOJ3046_R2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+        int R1 = sc.nextInt();
+        int S = sc.nextInt();
+        
+        int R2 = S*2 - R1;
+        System.out.println(R2);
+    }
+}
