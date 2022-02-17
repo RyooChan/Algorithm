8245a4b0eb70ca14c6f7e2c6864070a392e00530
RyooChan <53744363+RyooChan@users.noreply.github.com>
2022-02-17 16:57:11 +0900

    Create BOJ9655_돌게임.java

---
+++ java/220217/BOJ9655_돌게임.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        
+        if(N%2==1) System.out.println("SK");
+        else System.out.println("CY");
+        
+    }
+}
