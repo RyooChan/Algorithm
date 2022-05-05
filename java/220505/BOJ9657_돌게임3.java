cc2047d9d81a4c3d709814cace7f780325e3a0d7
RyooChan <53744363+RyooChan@users.noreply.github.com>
2022-05-05 16:20:45 +0900

    Create BOJ9657_돌게임3.java

---
+++ java/220505/BOJ9657_돌게임3.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int dp[] = new int[N+1];
+        
+        if(N<=4){
+            if(N==1) System.out.println("SK");
+            else if(N==2) System.out.println("CY");
+            else if(N==3) System.out.println("SK");
+            else System.out.println("SK");
+            return;
+        }
+        
+        dp[1] = 1;
+        dp[2] = 0;
+        dp[3] = 1;
+        dp[4] = 1;
+        
+        for(int i=5; i<N+1; i++){
+            int win = dp[i-4] + dp[i-3] + dp[i-1];
+            if(win<3){
+                dp[i] = 1;
+            }else{
+                dp[i] = 0;
+            }
+        }
+        
+        if(dp[N]==1) System.out.println("SK");
+        else System.out.println("CY");
+        
+    }
+}
