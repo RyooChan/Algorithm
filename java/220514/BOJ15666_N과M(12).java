2069c605a264e64e4ce41ac3f73b0c486fe8e77a
RyooChan <53744363+RyooChan@users.noreply.github.com>
2022-05-14 20:48:55 +0900

    Create BOJ15666_N과M(12).java

---
+++ java/220514/BOJ15666_N과M(12).java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+    public static int N, M;
+    public static int num[];
+    // public static boolean check[];
+    public static StringBuilder sb;
+    public static HashSet<String> cut = new HashSet<>();
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        sb = new StringBuilder();
+        
+        N = sc.nextInt();
+        M = sc.nextInt();
+        
+        num = new int[N];
+        // check = new boolean[N];
+        for(int i=0; i<N; i++) num[i] = sc.nextInt();
+        Arrays.sort(num);
+        
+        for(int i=0; i<N; i++){
+            // check[i] = true;
+            maker(i, 1, num[i] + "");
+            // check[i] = false;
+        } 
+        System.out.println(sb);
+      
+    }
+    public static void maker(int now, int depth, String str){
+        if(depth == M){
+            if(!cut.contains(str)){
+                cut.add(str);
+                sb.append(str + "\n");
+            }
+            return;
+        }
+        
+        for(int i=now; i<N; i++){
+            // if(check[i]) continue;
+            // check[i] = true;
+            maker(i, depth+1, str + " " + num[i]);
+            // check[i] = false;
+        }
+        
+    }
+}
