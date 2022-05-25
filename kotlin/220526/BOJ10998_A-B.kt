627d48f4b6b9e96a3c049962ef1d3b0578990d13
RyooChan <53744363+RyooChan@users.noreply.github.com>
2022-05-26 07:52:54 +0900

    Create BOJ10998_A-B.kt

---
+++ kotlin/220526/BOJ10998_A-B.kt
@@ -0,0 +1,10 @@
+import java.util.*;
+
+fun main(args: Array<String>) {
+    val sc: Scanner = Scanner(System.`in`);
+    var sum : Int = 0;
+    val A : Int = sc.nextInt();
+    val B : Int = sc.nextInt();
+    sum = A - B;
+    println(sum);
+}
