7ec7ce7a54748bb3b34ea1ae3c9d657d3ecf8bf4
RyooChan <53744363+RyooChan@users.noreply.github.com>
2022-05-26 08:03:04 +0900

    Create BOJ1008.kt

---
+++ kotlin/220526/BOJ1008.kt
@@ -0,0 +1,10 @@
+import java.util.*;
+
+fun main(args: Array<String>) {
+    val sc: Scanner = Scanner(System.`in`);
+    var sum : Double;
+    val A : Int = sc.nextInt();
+    val B : Int = sc.nextInt();
+    sum = (A.toDouble() / B.toDouble());
+    println(sum);
+}
