e86d20536462c3f493ce66194f979433214e1e25
RyooChan <53744363+RyooChan@users.noreply.github.com>
2022-05-26 08:07:00 +0900

    Create BOJ10926_??!.kt

---
+++ kotlin/220526/BOJ10926_??!.kt
@@ -0,0 +1,9 @@
+import java.util.*;
+
+fun main(args: Array<String>) {
+    val sc: Scanner = Scanner(System.`in`);
+    val input : String = sc.nextLine();
+    
+    val ans = input + "??!";
+    println(ans);
+}
