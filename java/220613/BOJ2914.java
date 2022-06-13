c196679d6cc1e48c1d50da2a026824bf4304695f
RyooChan <53744363+RyooChan@users.noreply.github.com>
2022-06-13 22:24:33 +0900

    Create BOJ2914.java

---
+++ java/220613/BOJ2914.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int I = sc.nextInt();
+        System.out.println(A*(I-1)+1);
+    }
+}
