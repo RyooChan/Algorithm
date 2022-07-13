0a62c63c6b066bd10b2cf6a4a5f6749633aeb337
RyooChan <53744363+RyooChan@users.noreply.github.com>
2022-07-13 14:22:10 +0900

    Create BOJ_1008.go

---
+++ GO/BOJ_1008.go
@@ -0,0 +1,13 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	var A float64
+	var B float64
+
+	fmt.Scanln(&A, &B)
+	fmt.Println(float64(A / B))
+}
