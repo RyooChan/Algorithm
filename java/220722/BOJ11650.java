49cd34024a61c6ad4a5b21add61105fdef0949ff
RyooChan <53744363+RyooChan@users.noreply.github.com>
2022-07-22 17:07:32 +0900

    Create BOJ11650.java

---
+++ java/220722/BOJ11650.java
@@ -0,0 +1,45 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.util.*;
+
+class info implements Comparable<info>{
+    int x;
+    int y;
+    
+    info(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    @Override
+    public int compareTo(info o) {
+        if(this.x == o.x){
+            return this.y - o.y;
+        }
+        return this.x - o.x;
+    }
+}
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		PriorityQueue<info> pq = new PriorityQueue<>();
+		int N = sc.nextInt();
+		
+		for(int i=0; i<N; i++) pq.add(new info(sc.nextInt(), sc.nextInt()));
+		
+		while(!pq.isEmpty()){
+		    info info = pq.remove();
+		    System.out.println(info.x + " " + info.y);
+		}
+		
+	}
+}
