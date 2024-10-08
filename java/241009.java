0f635702c54d3df7f2fd6eb0a2b89ea96ab1ddbb
RyooChan <53744363+RyooChan@users.noreply.github.com>
2024-10-09 00:14:11 +0900

    Create 241009.java

---
+++ java/241009.java
@@ -0,0 +1,27 @@
+/**
+https://www.hackerrank.com/challenges/2d-array/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
+[HackerRank] 2d-array java 풀이
+*/
+
+private static final int xMove[] = {-1,-1, -1, 0, 1, 1, 1};
+private static final int yMove[] = {-1, 0, 1, 0, -1, 0, 1};
+
+public static int hourglassSum(List<List<Integer>> arr) {
+    int ans = Integer.MIN_VALUE;
+    
+    for(int i=1; i<6-1; i++) {
+        for(int j=1; j<6-1; j++) {
+            int sum = 0;
+            
+            for(int k=0; k<xMove.length; k++) {
+                sum += arr.get(i + xMove[k]).get(j + yMove[k]);
+            }
+            
+            ans = Math.max(sum, ans);
+            System.out.println("index : " + i + ":" + j + ", location : " + sum);
+        }
+    }
+    
+    return ans;
+}
