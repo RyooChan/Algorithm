0031bf071f4d1e992b52387a6dba1b1dfd1d0ab1
RyooChan <53744363+RyooChan@users.noreply.github.com>
2025-12-27 00:07:30 +0900

    Create LC155. Min Stack.java

---
+++ java/LC155. Min Stack.java
@@ -0,0 +1,39 @@
+class MinStack {
+    Stack<Integer> minStack = new Stack<>();
+    Stack<Integer> stack;
+
+    public MinStack() {
+        stack = new Stack<Integer>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int removed = stack.pop();
+        if (removed == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
